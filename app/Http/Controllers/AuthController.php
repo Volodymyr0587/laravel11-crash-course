8db54ce114b14645e4c3ceb22522ded68e60c601
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-06-17 21:11:59 +0300

    Register user

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,25 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     public function register(Request $request)
     {
        // Validate
-       $request->validate([
+       $fields = $request->validate([
             'username' => ['required', 'max:255'],
-            'email' => ['required', 'email', 'max:255'],
+            'email' => ['required', 'email', 'unique:users,email', 'max:255'],
             'password' => ['required', 'confirmed', 'min:3']
        ]);
-
-       dd('ok');
-
        // Register
-
+       $user = User::create($fields);
        // Login
-
+       Auth::login($user);
        // Redirect
+       return redirect()->route('home');
+
     }
 }
