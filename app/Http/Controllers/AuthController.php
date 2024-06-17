1c7c44f19bf9d38772a46ee6cd09fc63eddcad51
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-06-17 18:57:25 +0300

    Validation

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,6 +9,13 @@ class AuthController extends Controller
     public function register(Request $request)
     {
        // Validate
+       $request->validate([
+            'username' => ['required', 'max:255'],
+            'email' => ['required', 'email', 'max:255'],
+            'password' => ['required', 'confirmed', 'min:3']
+       ]);
+
+       dd('ok');
 
        // Register
 
