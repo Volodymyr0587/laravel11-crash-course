9fee4678cd183076af80593145528672f903aeb1
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-06-19 18:31:26 +0300

    Factory & Seeder

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -22,7 +22,7 @@ class AuthController extends Controller
         // Login
         Auth::login($user);
         // Redirect
-        return redirect()->route('home');
+        return redirect()->route('dashboard');
     }
 
     public function login(Request $request)
